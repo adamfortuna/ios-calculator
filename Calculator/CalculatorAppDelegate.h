84ec2965637da20a31b6c785637835b677bd80ef
Adam Fortuna <adam@fortuna.name>
2012-08-28 19:38:13 -0400

    Initial Commit

---
+++ Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by Adam Fortuna on 8/28/12.
+//  Copyright (c) 2012 Adam Fortuna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Adam Fortuna on 8/28/12.
+//  Copyright (c) 2012 Adam Fortuna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end

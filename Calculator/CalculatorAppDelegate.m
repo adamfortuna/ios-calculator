48b595073069b3473eb450257a9618355c8fe921
Adam Fortuna <adam@fortuna.name>
2012-09-10 17:53:15 -0400

    Reverted color changes

--- Calculator/CalculatorAppDelegate.m
+++ Calculator/CalculatorAppDelegate.m
@@ -13,8 +13,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    // [[UIButton appearance] setBackgroundColor:[UIColor redColor]];
-    
     return YES;
 }
 							
